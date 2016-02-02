abe1e21afe0ef0d38aa17ad755a000624a1aba66
vladnau89 <vladnau89@yandex.ru>
2016-02-03 00:55:24 +0300

    add Color class

---
+++ Color.py
@@ -0,0 +1,19 @@
+
+class Color:
+    def __init__(self, r, g, b, a):
+        self.__r = r
+        self.__g = g
+        self.__b = b
+        self.__a = a
+
+    def r(self):
+        return self.__r
+
+    def g(self):
+        return self.__g
+
+    def b(self):
+        return self.__b
+
+    def a(self):
+        return self.__a
