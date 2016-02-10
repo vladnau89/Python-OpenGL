6c03afb0802ce3df2d0fda81377f980ba4d9d5b0
vladnau89 <vladnau89@yandex.ru>
2016-02-10 23:46:04 +0300

    african_head.obj rasterazation

--- Main.py
+++ Main.py
@@ -2,39 +2,38 @@ from TGAImage import TGAImage, Format
 from Color import Color
 from Model import Model
 from Vector import Vector
+from random import randint
 
 red = Color(255, 0, 0, 255)
 green = Color(0, 255, 0, 255)
 white = Color(255, 255, 255, 255)
 
-width = 400
-height = 400
+width = 700
+height = 700
 
 def main():
-    # model = Model("obj/african_head.obj")
+    model = Model("obj/african_head.obj")
     image = TGAImage(width + 1, height + 1, Format.RGBA)
 
     #model.write("test.txt")
 
-    # for face in model.faces:
-    #     for i in range(0, 3):
-    #         v0 = model.verts[int(face.get(i))]
-    #         v1 = model.verts[int(face.get((i + 1) % 3))]
-    #         x0 = (v0.x + 1.) * width / 2.
-    #         y0 = (v0.y + 1.) * height/2.
-    #         x1 = (v1.x + 1.) * width/2.
-    #         y1 = (v1.y + 1.) * height/2.
-    #         line(int(x0), int(y0), int(x1), int(y1), image, white)
-
+    for face in model.faces:
+        screen_coord =[Vector(), Vector(), Vector()]
+        for i in range(0, 3):
+            world_coord_vector = model.verts[int(face.get(i))]
+            screen_coord[i].x = int((world_coord_vector.x + 1.)*width/2)
+            screen_coord[i].y = int((world_coord_vector.y + 1.)*height/2)
+        random_color = Color(randint(0, 255), randint(0, 255), randint(0, 255), 255)
+        triangle(screen_coord[0], screen_coord[1], screen_coord[2], image, random_color)
     #line(20, 13, 40, 80, image, red)
     #line(0, 0, width, height, image, red)
     #line(40, 80, 20, 13, image, red)
+    #
+    # t0 = [Vector(10, 70), Vector(50, 160), Vector(70, 80)]
+    # t1 = [Vector(180, 50), Vector(150, 1), Vector(70, 180)]
+    # t2 = [Vector(180, 150), Vector(120, 160), Vector(130, 180)]
 
-    t0 = [Vector(10, 70), Vector(50, 160), Vector(70, 80)]
-    t1 = [Vector(180, 50), Vector(150, 1), Vector(70, 180)]
-    t2 = [Vector(180, 150), Vector(120, 160), Vector(130, 180)]
-
-    triangle(t0[0], t0[1], t0[2], image, white)
+    # triangle(t0[0], t0[1], t0[2], image, white)
     # triangle(t1[0], t1[1], t1[2], image, white)
     # triangle(t2[0], t2[1], t2[2], image, green)
 
@@ -42,9 +41,9 @@ def main():
 
 
 def triangle(v1, v2, v3, image, color):
-    line_by_vector(v1, v2, image, color)
-    line_by_vector(v1, v3, image, color)
-    line_by_vector(v2, v3, image, color)
+    # line_by_vector(v1, v2, image, red)
+    # line_by_vector(v1, v3, image, red)
+    # line_by_vector(v2, v3, image, red)
 
     if v1.y > v2.y:
         v1, v2 = v2, v1
@@ -55,7 +54,7 @@ def triangle(v1, v2, v3, image, color):
     if v1.y > v3.y:
         v1, v3 = v3, v1
 
-    total_height = v3.y - v1.y
+    total_height = int(v3.y - v1.y)
 
     for y in range(0, total_height, 1):
         is_second_half = y >= v2.y - v1.y
